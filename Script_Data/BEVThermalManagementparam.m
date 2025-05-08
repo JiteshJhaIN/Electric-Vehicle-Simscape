f587feb6f6fa89a69b4e2ed1e1f41afbd97d9352
Jitesh Jha <123732167+JiteshJhaIN@users.noreply.github.com>
2025-05-08 13:58:51 +0530

    Update BEVThermalManagementparam.m

--- Script_Data/BEVThermalManagementparam.m
+++ Script_Data/BEVThermalManagementparam.m
@@ -11,7 +11,7 @@ vehicleThermal.cabin_CO2_init = 4e-4; % Initial CO2 mole fraction
 
 %% Vehicle Cabin
 
-vehicleThermal.cabin_duct_area = 0.04; % [m^2] Air duct cross-sectional area
+vehicleThermal.cabin_duct_area = 0.04; % [m^2] A ir duct cross-sectional area
 vehicleThermal.cabin_duct_volume= 0.0015; % [m^3] Aier duct volume
 vehicleThermal.cabin_perPersonCO2 = 0.01; % [g/s] Adult CO2 exhale rate 
 vehicleThermal.cabin_perPersonMoisture = 0.04; % [g/s] Adult moisture exhale rate
